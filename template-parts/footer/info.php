05b0d657e90936fc8467b20b7377f2c131f0613c
Andrew Taylor <andrew@ataylor.me>
2019-06-25 16:52:59 +0200

    Clean up coding standards

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -11,14 +11,15 @@ namespace WP_Rig\WP_Rig;
 
 <div class="site-info">
 	<?php
-	$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ) . '">WordPress</a>';
-	$wprig_link = '<a href="' . esc_url( __( 'https://wprig.io/', 'wp-rig' ) ) . '">WP Rig</a>';
-	printf( esc_html__( 'Proudly powered by %1$s and %2$s', 'wp-rig' ), $wordpress_link, $wprig_link );
+	$wordpress_link = esc_attr( '<a href="' . esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ) . '">WordPress</a>' );
+	$wprig_link = esc_attr( '<a href="' . esc_url( __( 'https://wprig.io/', 'wp-rig' ) ) . '">WP Rig</a>' );
+	/* translators: %1$s is the content management system (WordPress) and %2$s is the theme framework (WP Rig) */
+	esc_attr( sprintf( esc_html__( 'Proudly powered by %1$s and %2$s', 'wp-rig' ), $wordpress_link, $wprig_link ) );
 
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
 		the_privacy_policy_link( '<span class="sep"> | </span>' );
 	}
 	?>
 	<span class="sep"> | </span>
-	&copy; <?php echo date('Y'); ?> Andrew Taylor
+	&copy; <?php echo esc_attr( date( 'Y' ) ); ?> Andrew Taylor
 </div><!-- .site-info -->
