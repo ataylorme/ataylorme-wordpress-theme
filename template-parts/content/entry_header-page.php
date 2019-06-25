1d2cbe9c0927e66a9e1fd788f5aa6e8233317b71
Andrew Taylor <andrew@ataylor.me>
2019-06-25 04:30:43 +0200

    Add page entry header
    Without post tile and meta

---
+++ template-parts/content/entry_header-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying a post's header
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+<header class="entry-header">
+	<?php
+	if ( ! is_search() && ! is_front_page() ) {
+		get_template_part( 'template-parts/content/entry_thumbnail', get_post_type() );
+	}
+	?>
+</header><!-- .entry-header -->
