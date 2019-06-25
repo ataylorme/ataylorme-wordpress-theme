dfbbdc3d9e89fde2a35b214eb03b230e63d87323
Andrew Taylor <andrew@ataylor.me>
2019-06-25 15:55:28 +0200

    Update footer site info

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -10,19 +10,15 @@ namespace WP_Rig\WP_Rig;
 ?>
 
 <div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ); ?>">
-		<?php
-		/* translators: %s: CMS name, i.e. WordPress. */
-		printf( esc_html__( 'Proudly powered by %s', 'wp-rig' ), 'WordPress' );
-		?>
-	</a>
-	<span class="sep"> | </span>
 	<?php
-	/* translators: Theme name. */
-	printf( esc_html__( 'Theme: %s by the contributors.', 'wp-rig' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>' );
+	$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org/', 'wp-rig' ) ) . '">WordPress</a>';
+	$wprig_link = '<a href="' . esc_url( __( 'https://wprig.io/', 'wp-rig' ) ) . '">WP Rig</a>';
+	printf( esc_html__( 'Proudly powered by %1$s and %2$s', 'wp-rig' ), $wordpress_link, $wprig_link );
 
 	if ( function_exists( 'the_privacy_policy_link' ) ) {
 		the_privacy_policy_link( '<span class="sep"> | </span>' );
 	}
 	?>
+	<span class="sep"> | </span>
+	&copy; <?php echo date('Y'); ?> Andrew Taylor
 </div><!-- .site-info -->
