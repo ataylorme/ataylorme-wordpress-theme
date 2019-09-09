7ebee7dcc3ad1316bf9d7d241aa473037febba60
Andrew Taylor <andrew@ataylor.me>
2019-09-09 11:12:37 -0700

    Add template for presentations

---
+++ template-parts/content/entry_content-presentations.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template part for displaying a post's content
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+<div class="entry-content">
+	<?php
+	the_content(
+		sprintf(
+			wp_kses(
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'wp-rig' ),
+				[
+					'span' => [
+						'class' => [],
+					],
+				]
+			),
+			get_the_title()
+		)
+	);
+	?>
+</div><!-- .entry-content -->
---
+++ template-parts/content/entry_meta-presentations.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template part for displaying a post's metadata
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+$post_type_obj = get_post_type_object( get_post_type() );
+?>
+<div class="entry-meta">
+
+</div><!-- .entry-meta -->
+<?php
